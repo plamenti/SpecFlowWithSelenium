8af2be8b7748155e111d2be42a901aca6539bd7f
plamenti <plamenti_@abv.bg>
2018-08-25 17:58:22 +0300

    Add SearchForBooksSteps class

---
+++ SeleniumTaskAmazon/StepDefinitions/SearchForBooksSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SeleniumTaskAmazon.StepDefinitions
+{
+    [Binding]
+    public class SearchForBooksSteps
+    {
+        [When(@"I select section books")]
+        public void WhenISelectSectionBooks()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"Search for book with title (.*)")]
+        public void WhenSearchForBookWithTitle(string title)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"First item has following attributes")]
+        public void ThenFirstItemHasFollowingAttributes(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
