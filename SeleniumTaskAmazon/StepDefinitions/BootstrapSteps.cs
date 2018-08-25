a442c3a72aa4134dbf032b64ce89c9401ddf96ea
plamenti <plamenti_@abv.bg>
2018-08-25 17:29:10 +0300

    Implement BootstrapSteps ad SelectBookSteps step definitions

---
+++ SeleniumTaskAmazon/StepDefinitions/BootstrapSteps.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumTaskAmazon.Helpers;
+using TechTalk.SpecFlow;
+
+namespace SeleniumTaskAmazon.StepDefinitions
+{
+    [Binding]
+    public class BootstrapSteps
+    {
+        private IWebDriver driver;
+        private IWait<IWebDriver> wait;
+
+        [BeforeScenario]
+        public void setupDriver()
+        {
+            driver = DriverManager.GetDriver();
+            wait = WaitManager.GetDefaultWait(driver);
+            ScenarioContext.Current.Set<IWebDriver>(driver);
+            ScenarioContext.Current.Set<IWait<IWebDriver>>(wait);
+        }
+
+        [AfterScenario]
+        public void tearDownDriver()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
---
+++ SeleniumTaskAmazon/StepDefinitions/SelectBookSteps.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumTaskAmazon.Helpers;
+using SeleniumTaskAmazon.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumTaskAmazon.StepDefinitions
+{
+    [Binding]
+    public class SelectBookSteps
+    {
+        private HomePage page;
+        private IWebDriver driver = ScenarioContext.Current.Get<IWebDriver>();
+        private IWait<IWebDriver> wait = ScenarioContext.Current.Get<IWait<IWebDriver>>();
+
+        [Given(@"I navigate to Amazon book store in UK")]
+        public void GivenINavigateToAmazonBookStoreInUK()
+        {
+            page = new HomePage(driver, wait);
+            page.NavigateTo();
+        }
+
+        [Given(@"I am not logged in")]
+        public void GivenIAmNotLoggedIn()
+        {
+            string greetingLabelName = "Sign in";
+            Assert.True(page.isLoggedInAs(greetingLabelName), $"Greeting label should contain: {greetingLabelName}");
+        }
+
+        [Then(@"The correct page is open")]
+        public void ThenTheCorrectPageIsOpen()
+        {
+            string expectedUrl = @"https://www.amazon.co.uk/";
+            string expectedTitle = "Amazon.co.uk: Low Prices in Electronics, Books, Sports Equipment & more";
+
+            Assert.AreEqual(expectedUrl, page.getCurrentUrl());
+            Assert.AreEqual(expectedTitle, page.getTitle());
+            Assert.True(page.IsAt());
+        }
+
+        [Then(@"I can start to search for books")]
+        public void ThenICanStartToSearchForBooks()
+        {
+            Assert.True(page.canSearch());
+        }
+    }
+}
