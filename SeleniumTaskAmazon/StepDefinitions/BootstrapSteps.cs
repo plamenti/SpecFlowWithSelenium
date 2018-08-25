1fc47b79f63ef422805df12a408360764ac5ff3f
plamenti <plamenti_@abv.bg>
2018-08-25 19:02:03 +0300

    Refactor BootstrapSteps

--- SeleniumTaskAmazon/StepDefinitions/BootstrapSteps.cs
+++ SeleniumTaskAmazon/StepDefinitions/BootstrapSteps.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using SeleniumTaskAmazon.Helpers;
+using SeleniumTaskAmazon.Pages;
 using TechTalk.SpecFlow;
 
 namespace SeleniumTaskAmazon.StepDefinitions
@@ -10,14 +11,17 @@ namespace SeleniumTaskAmazon.StepDefinitions
     {
         private IWebDriver driver;
         private IWait<IWebDriver> wait;
+        private HomePage page;
 
         [BeforeScenario]
         public void setupDriver()
         {
             driver = DriverManager.GetDriver();
             wait = WaitManager.GetDefaultWait(driver);
+            page = new HomePage(driver, wait);
             ScenarioContext.Current.Set<IWebDriver>(driver);
             ScenarioContext.Current.Set<IWait<IWebDriver>>(wait);
+            ScenarioContext.Current.Set<HomePage>(page);
         }
 
         [AfterScenario]
