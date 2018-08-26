6174ef8093fbebd67ae805a498ae5c46de657203
plamenti <plamenti_@abv.bg>
2018-08-26 23:22:16 +0300

    Create AssertionsManager

---
+++ SeleniumTaskAmazon/Helpers/ScreenShotsManager.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.IO;
+
+namespace SeleniumTaskAmazon.Helpers
+{
+    public class ScreenShotsManager
+    {
+        public static String CreateScreenshot(IWebDriver driver)
+        {
+            string uuid = Guid.NewGuid().ToString();
+            string fileName = Path.Combine(IOManager.CreateImagesDirectory(), uuid) + ".png";
+
+            Screenshot screen = ((ITakesScreenshot)driver).GetScreenshot();
+            screen.SaveAsFile(fileName, ScreenshotImageFormat.Png);
+
+            return fileName;
+        }
+    }
+}
