5b2d1bc94c057d053a4a9ab554a0db13eaee7055
plamenti <plamenti_@abv.bg>
2018-08-26 23:21:39 +0300

    Create IOManager

---
+++ SeleniumTaskAmazon/Helpers/IOManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace SeleniumTaskAmazon.Helpers
+{
+    public class IOManager
+    {
+        private const string ImagesFolderPartialName = "Images";
+        private const string ReportsFolderPartialNameKey = "reports";
+
+        public static string CreateReportsDirectory()
+        {
+            string folderName = GetReportsFolderName();
+            try
+            {
+                if (!Directory.Exists(folderName))
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+            }
+            catch (IOException ex)
+            {
+
+            }
+
+            return folderName;
+        }
+
+        public static string CreateImagesDirectory()
+        {
+            string folderName = GetImagesFolderName();
+
+            try
+            {
+                if (!Directory.Exists(folderName))
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+            }
+            catch (IOException ex)
+            {
+
+            }
+
+            return folderName;
+
+        }
+
+        private static string GetReportsFolderName()
+        {
+            string projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string reportsFolderName = ConfigurationManager.AppSettings.Get(ReportsFolderPartialNameKey);
+            string folderName = Path.Combine(projectPath, reportsFolderName);
+            return folderName;
+        }
+
+        private static string GetImagesFolderName()
+        {
+            string reportsFolder = GetReportsFolderName();
+            string folderName = Path.Combine(reportsFolder, ImagesFolderPartialName);
+
+            return folderName;
+        }
+    }
+}
