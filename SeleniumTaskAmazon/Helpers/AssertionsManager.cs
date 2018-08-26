f806865c4bffbe657d03e49a667bf3d926c04ab0
plamenti <plamenti_@abv.bg>
2018-08-26 23:22:45 +0300

    Create AssertionsManager

---
+++ SeleniumTaskAmazon/Helpers/AssertionsManager.cs
@@ -0,0 +1,107 @@
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumTaskAmazon.Helpers
+{
+    public class AssertionsManager
+    {
+        public static void IsTrue(IWebDriver driver, ExtentTest extentTest, bool assertedValue, string reportingMessage)
+        {
+            try
+            {
+                Assert.IsTrue(assertedValue);
+                extentTest.Pass(reportingMessage);
+            }
+            catch (AssertionException)
+            {
+                extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "'", MediaEntityBuilder.CreateScreenCaptureFromPath(ScreenShotsManager.CreateScreenshot(driver)).Build());
+                throw;
+            }
+        }
+
+        public static void AreEqual(IWebDriver driver, ExtentTest extentTest, string expectedValue, string actualValue, string reportingMessage)
+        {
+            try
+            {
+                Assert.AreEqual(expectedValue, actualValue);
+                extentTest.Pass(reportingMessage);
+            }
+            catch (AssertionException)
+            {
+                if (driver != null)
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "'", MediaEntityBuilder.CreateScreenCaptureFromPath(ScreenShotsManager.CreateScreenshot(driver)).Build());
+                }
+                else
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "'");
+                }
+                throw;
+            }
+        }
+
+        public static void AreEqual(IWebDriver driver, ExtentTest extentTest, int expectedValue, int actualValue, string reportingMessage)
+        {
+            try
+            {
+                Assert.AreEqual(expectedValue, actualValue);
+                extentTest.Pass(reportingMessage);
+            }
+            catch (AssertionException)
+            {
+                if (driver != null)
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue, MediaEntityBuilder.CreateScreenCaptureFromPath(ScreenShotsManager.CreateScreenshot(driver)).Build());
+                }
+                else
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue);
+                }
+                throw;
+            }
+        }
+
+        public static void AreEqual(IWebDriver driver, ExtentTest extentTest, double expectedValue, double actualValue, string reportingMessage)
+        {
+            try
+            {
+                Assert.AreEqual(expectedValue, actualValue);
+                extentTest.Pass(reportingMessage);
+            }
+            catch (AssertionException)
+            {
+                if (driver != null)
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue, MediaEntityBuilder.CreateScreenCaptureFromPath(ScreenShotsManager.CreateScreenshot(driver)).Build());
+                }
+                else
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue);
+                }
+                throw;
+            }
+        }
+
+        public static void AreEqual(IWebDriver driver, ExtentTest extentTest, object expectedValue, object actualValue, string reportingMessage)
+        {
+            try
+            {
+                Assert.AreEqual(expectedValue, actualValue);
+                extentTest.Pass(reportingMessage);
+            }
+            catch (AssertionException)
+            {
+                if (driver != null)
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue, MediaEntityBuilder.CreateScreenCaptureFromPath(ScreenShotsManager.CreateScreenshot(driver)).Build());
+                }
+                else
+                {
+                    extentTest.Fail("Failure occurred when executing check '" + reportingMessage + "', actual value was " + actualValue);
+                }
+                throw;
+            }
+        }
+    }
+}
