491ea29caddc18643659ed76e8a1651687feba68
plamenti <plamenti_@abv.bg>
2018-08-26 09:43:06 +0300

    Create BookDetailsPage class

---
+++ SeleniumTaskAmazon/Pages/BookDetailsPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumTaskAmazon.Pages
+{
+    public class BookDetailsPage : BasePage
+    {
+        public BookDetailsPage(IWebDriver driver, IWait<IWebDriver> wait) : base(driver, wait)
+        {
+        }
+
+        public override bool IsAt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
