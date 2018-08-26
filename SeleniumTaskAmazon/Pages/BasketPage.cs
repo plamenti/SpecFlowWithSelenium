2128e5fc7a3f853f8e20b6e4f252acdc42cc0fc0
plamenti <plamenti_@abv.bg>
2018-08-26 13:58:51 +0300

    Add BaksetPage elements By fields

--- SeleniumTaskAmazon/Pages/BasketPage.cs
+++ SeleniumTaskAmazon/Pages/BasketPage.cs
@@ -6,6 +6,12 @@ namespace SeleniumTaskAmazon.Pages
 {
     public class BasketPage : BasePage
     {
+        private By proceedToCkeckoutButton = By.Id("hlb-ptc-btn-native");
+        private By editBasketButton = By.Id("hlb-view-cart-announce");
+        private By confirmationLabel = By.XPath("//div[contains(@id, 'order-row-confirm')]/h1");
+        private By bookTumbnail = By.Id("huc-v2-order-row-images");
+        private By bookTitle = By.XPath("//div[@id='mdp-title']//span[contains(@class, 'product-title')]");
+
         public BasketPage(IWebDriver driver, IWait<IWebDriver> wait) : base(driver, wait)
         {
         }
