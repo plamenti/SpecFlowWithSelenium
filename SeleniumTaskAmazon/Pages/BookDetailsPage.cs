c5bd023425bf5ce6638b41b6210f4b78dac0e526
plamenti <plamenti_@abv.bg>
2018-08-26 13:06:27 +0300

    Implement BookDetailsPage

--- SeleniumTaskAmazon/Pages/BookDetailsPage.cs
+++ SeleniumTaskAmazon/Pages/BookDetailsPage.cs
@@ -1,11 +1,17 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using SeleniumTaskAmazon.Models;
 
 namespace SeleniumTaskAmazon.Pages
 {
     public class BookDetailsPage : BasePage
     {
+        private By tile = By.Id("productTitle");
+        private By badge = By.XPath("//div[contains(@id, 'Badge')]//i");
+        private By price = By.XPath("//li[contains(@class,' selected')]//a[@role='button']/span[2]");
+        private By format = By.XPath("//li[contains(@class,' selected')]//a[@role='button']/span[1]");
+
         public BookDetailsPage(IWebDriver driver, IWait<IWebDriver> wait) : base(driver, wait)
         {
         }
@@ -14,5 +20,62 @@ namespace SeleniumTaskAmazon.Pages
         {
             throw new NotImplementedException();
         }
+
+        public string GetTitle()
+        {
+            return GetElementText(tile);
+        }
+
+        public bool GetBadge()
+        {
+            bool foundBadge;
+
+            try
+            {
+                IWebElement badgeElement = GetElement(badge);
+                foundBadge = true;
+            }
+            catch (NoSuchElementException)
+            {
+                foundBadge = false;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                foundBadge = false;
+            }
+
+            return foundBadge;
+    }
+
+    public string GetFormat()
+        {
+            IWebElement foundElement = GetElement(format); ;
+
+            return foundElement.Text;
+        }
+
+        public double GetPrice()
+        {
+            string foundPriceAsText = GetElement(price).Text;
+            double priceValue = ParcePrice(foundPriceAsText);
+
+            return priceValue;
+        }
+
+        public Book GetBook()
+        {
+            Format foundItemFormat;
+            Enum.TryParse(GetFormat(), out foundItemFormat);
+
+            return new Book
+            {
+                Title = GetTitle(),
+                Price = GetPrice(),
+                Badge = GetBadge(),
+                Format = foundItemFormat
+            };
+        }
+
     }
 }
+
