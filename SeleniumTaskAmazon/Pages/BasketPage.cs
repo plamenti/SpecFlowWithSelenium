9552b020f7261ef225e0e03aac9bd9c845f8ede5
plamenti <plamenti_@abv.bg>
2018-08-26 14:43:19 +0300

    Add step definitions to BasketOperationsSteps, implement IsAt and GetItemsCount methods in BasketPage

--- SeleniumTaskAmazon/Pages/BasketPage.cs
+++ SeleniumTaskAmazon/Pages/BasketPage.cs
@@ -6,6 +6,7 @@ namespace SeleniumTaskAmazon.Pages
 {
     public class BasketPage : BasePage
     {
+        private By orderLabelContainer = By.Id("huc-v2-order-row-inner");
         private By proceedToCkeckoutButton = By.Id("hlb-ptc-btn-native");
         private By editBasketButton = By.Id("hlb-view-cart-announce");
         private By confirmationLabel = By.XPath("//div[contains(@id, 'order-row-confirm')]/h1");
@@ -18,7 +19,27 @@ namespace SeleniumTaskAmazon.Pages
 
         public override bool IsAt()
         {
+            bool isOrderLabelContainerVsible = CheckElementIsVisible(orderLabelContainer);
+            bool isEditBasketButtonVisible = CheckElementIsVisible(editBasketButton);
+            bool isConfirmationLabelVisible = CheckElementIsVisible(confirmationLabel);
+            bool isProceedToCkeckoutButtonVisible = CheckElementIsVisible(proceedToCkeckoutButton);
+            bool isBookTumbnailVisible = CheckElementIsVisible(bookTumbnail);
+
+            return isOrderLabelContainerVsible &&
+                isEditBasketButtonVisible &&
+                isConfirmationLabelVisible &&
+                isProceedToCkeckoutButtonVisible &&
+                isBookTumbnailVisible;
+        }
+
+        public int GetItemsCount()
+        {
             throw new NotImplementedException();
+        }
+
+        public string GetLabelTitle()
+        {
+            return GetElementText(confirmationLabel).Trim();
         }
     }
 }
--- SeleniumTaskAmazon/StepDefinitions/BasketOperationsSteps.cs
+++ SeleniumTaskAmazon/StepDefinitions/BasketOperationsSteps.cs
@@ -1,4 +1,5 @@
-﻿using SeleniumTaskAmazon.Pages;
+﻿using NUnit.Framework;
+using SeleniumTaskAmazon.Pages;
 using TechTalk.SpecFlow;
 
 namespace SeleniumTaskAmazon.StepDefinitions
@@ -9,6 +10,33 @@ namespace SeleniumTaskAmazon.StepDefinitions
         private BookDetailsPage bookDetailsPage = ScenarioContext.Current.Get<BookDetailsPage>();
         private BasketPage basketPage = ScenarioContext.Current.Get<BasketPage>();
 
+        [When(@"Add book to the basket")]
+        public void AddBookToTheBasket()
+        {
+            bookDetailsPage.AddBookToBasket();
+        }
+
+        [Then(@"The notification is shown")]
+        public void VerifyNotificationIsShown()
+        {
+            Assert.IsTrue(basketPage.IsAt(), "Notification Book is added to the basket is not operational");
+        }
+
+        [Then(@"Notification title is (.*)")]
+        public void VerifyTitle(string expectedTitle)
+        {
+            string actualTitle = basketPage.GetLabelTitle();
+
+            Assert.AreEqual(expectedTitle, actualTitle);
+        }
+
+        [Then(@"There is (.*) item in the basket")]
+        public void VerifyItemsCount(int expectedItemsCount)
+        {
+            int actualItemsCount = basketPage.GetItemsCount();
+
+            Assert.AreEqual(expectedItemsCount, actualItemsCount);
+        }
 
     }
 }
