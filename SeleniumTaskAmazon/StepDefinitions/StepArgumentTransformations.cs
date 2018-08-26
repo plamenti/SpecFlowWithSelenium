cb71424523b8417554b1915fd244b852638f296f
plamenti <plamenti_@abv.bg>
2018-08-26 13:10:05 +0300

    Create StepArgumentTransformations class

---
+++ SeleniumTaskAmazon/StepDefinitions/StepArgumentTransformations.cs
@@ -0,0 +1,14 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SeleniumTaskAmazon.StepDefinitions
+{
+    [Binding]
+    public class StepArgumentTransformations
+    {
+        [StepArgumentTransformation(@"(\d+)(?:st|nd|rd|th)")]
+        public int GetIndex(int index)
+        {
+            return index - 1;
+        }
+    }
+}
